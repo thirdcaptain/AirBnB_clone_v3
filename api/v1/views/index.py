2025ce05f90341eab6f9d7bdc6acf2be48145756
Isaac Wong <isaac@thirdcaptain.com>
2018-08-22 00:18:49 +0000

    Added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
     Create a route /status on the object app_views that reutns a JSON
 """
 
-
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -12,3 +12,14 @@ from flask import jsonify
 def status():
     json_status = {"status": "OK"}
     return jsonify(json_status)
+
+
+@app_views.route("/stats")
+def stats():
+    json_stats = {"amenities": storage.count("Amenity"),
+                  "cities": storage.count("City"),
+                  "places": storage.count("Place"),
+                  "reviews": storage.count("Review"),
+                  "states": storage.count("State"),
+                  "users": storage.count("User")}
+    return jsonify(json_stats)
