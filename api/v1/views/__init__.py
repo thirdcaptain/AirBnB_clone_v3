d1d1c262e368e3efd8a62b5926d3de8ed0055149
vmdn23 <victormdnguyen88@gmail.com>
2018-08-23 17:49:46 +0000

    fix pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,13 +9,13 @@ from flask import Blueprint
 
 app_views = Blueprint("app", __name__, url_prefix="/api/v1")
 
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
+if app_views:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
 """
 from api.v1.views.places_reviews import *
 """
