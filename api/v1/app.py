05aec412470ac2aba50b0b507d767352f52531fb
vmdn23 <victormdnguyen88@gmail.com>
2018-08-23 00:30:09 +0000

    enable HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,12 +8,14 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 from flask import make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
